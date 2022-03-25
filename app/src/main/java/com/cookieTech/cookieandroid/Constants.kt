c8a5db2d9fed5bf468cacf073186a53175a60d37
Akash shahriar <akashshahriar.kgs@gmail.com>
2022-03-25 13:50:03 +0600

    Revert "initial architecture setup"
    This reverts commit becf838c

--- app/src/main/java/com/cookieTech/cookieandroid/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.cookieTech.cookieandroid
-
-
-
-val MealType = arrayListOf("Breakfast","Lunch","Snacks","Dinner")
-const val UNIT_DATA_FILENAME = "unit.json"
--- app/src/main/java/com/cookieTech/cookieandroid/Data/Dao/UnitsDao.kt
+++
@@ -1,9 +0,0 @@
-package com.cookieTech.cookieandroid.Data.Dao
-
-import androidx.room.Dao
-import com.cookieTech.cookieandroid.Domain.Models.Units
-
-@Dao
-abstract class UnitsDao: BaseDao<Units>() {
-
-}
